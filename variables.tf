3587dba6673e81af63317fdcf33fa20aa4be8bd7
15srija <71713625+15srija@users.noreply.github.com>
2023-10-30 11:52:15 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "list-instances" {
+    default = ["instance-1","instance-2"]
+}
+variable "cidr-s" {
+    default = ["172.31.0.12","172.31.1.13"]
+}
+variable "subnet-ids" {
+  default = ["subnet-07a69964a0a9797ae","subnet-03e604eb39c18aafc"]
+}
